c226b96edcc3acf2ef37227c0b67195a46d6dc1f
Budddy <Budddyjellz@gmail.com>
2023-10-13 14:19:13 -0400

    Moving Saws

---
+++ Assets/Scripts/MovingTraps.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MovingTraps1 : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] points;
+    [SerializeField] private float speed = 0f;
+
+    private Rigidbody2D rb;
+    private int i = 0;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, points[i].transform.position) < 0.2f)
+        {
+            i++;
+        }
+        if (i == points.Length)
+        {
+            i = 0;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, points[i].transform.position, speed * Time.deltaTime);
+                
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        for(int i = 0; i < points.Length; i++)
+        {
+            Gizmos.DrawWireSphere(points[i].transform.position, 0.5f);
+            if (i != points.Length - 1)
+            {
+                Gizmos.DrawLine(points[i].transform.position, points[i + 1].transform.position);
+            }
+        }
+    }
+}
